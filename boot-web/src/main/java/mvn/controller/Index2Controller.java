a84059331989b135136ae5bbfe15b8fb49490a4c
GLgying <G_Lybbh@163.com>
2022-11-23 10:53:12 +0800

    git-boot

---
+++ boot-web/src/main/java/mvn/controller/Index2Controller.java
@@ -0,0 +1,21 @@
+package mvn.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author:TuoTuo
+ * @createDate:2022/11/23 10:16
+ * @description:
+ */
+@Slf4j
+@RestController("index2")
+public class Index2Controller {
+    @RequestMapping("/")
+    public String index(){
+        log.info("---");
+        log.info("---");
+        return "hello";
+    }
+}

11c5c57f52f290a6d2c7a93ad8b071560da7b570
GLgying <G_Lybbh@163.com>
2022-11-23 11:07:36 +0800

    git-boot

--- boot-web/src/main/java/mvn/controller/Index2Controller.java
+++ boot-web/src/main/java/mvn/controller/Index2Controller.java
@@ -15,7 +15,7 @@ public class Index2Controller {
     @RequestMapping("/")
     public String index(){
         log.info("---");
-        log.info("---");
+        log.info("-dev--");
         return "hello";
     }
 }
